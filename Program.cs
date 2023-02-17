df78f685ecb71f0ac4aee2b485b2218d495b8301
USER <USER@BRYAN>
2023-02-17 09:35:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Relen_Project
+{
+    internal class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+
+
+        }
+        [SetUp]
+        public void InitializeTest()
+        {
+            driver.Navigate().GoToUrl("https://www.relen.io");
+        }
+        [Test]
+        public void ExecuteTest()
+        {
+            driver.Manage().Window.Maximize();
+            IWebElement GetStarted = driver.FindElement(By.ClassName("text-decoration-none buttonn"));
+            GetStarted.Click();
+
+
+        }
+        //[TearDown]
+        //public void EndTest()
+        //{
+        //  driver.Quit();
+        //}
+
+
+    }
+}
+
+
+
+
